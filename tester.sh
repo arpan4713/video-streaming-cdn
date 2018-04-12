64bd38085c0328aa7b1662c1ca32663384042322
Andrew Wong <wongandrew97@gmail.com>
2018-04-12 00:04:29 -0400

    Geographical log fix

--- tester.sh
+++ tester.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 h3 python start_server.py 3;
 h4 python start_server.py 4;
 h2 ./nameserver/nameserver nsLog 4000 0 serverlist;
-h1 ./miProxy/miProxy miLog .75 1234 10.0.0.2 4000 & python launch_firefox.py 1;
+#h1 ./miProxy/miProxy miLog .75 1234 10.0.0.2 4000 & python launch_firefox.py 1;
 
