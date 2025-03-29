5540ee0c4a4b07a407bdab30233c59a68c368023
arpan4713 <153415169+arpan4713@users.noreply.github.com>
2025-03-29 10:49:41 +0530

    Update start_server.py

--- start_server.py
+++ start_server.py
@@ -2,11 +2,13 @@ import subprocess
 import sys
 
 if len(sys.argv) != 2:
-	print 'Error: Usage is python start_server.py <host_number>'
-	exit(1)
+    print('Error: Usage is python3 start_server.py <host_number>')
+    sys.exit(1)
 
 conf = sys.argv[1]
-if(int(conf) < 1 or int(conf) > 8):
-	print 'Error: <host_number> must be between 1 and 8'
-	exit(1)
-subprocess.call(["/usr/local/apache2/bin/apachectl", "-f", "/home/proj2/.confs/httpd" + conf + ".conf"])
+
+if not conf.isdigit() or not (1 <= int(conf) <= 8):
+    print('Error: <host_number> must be between 1 and 8')
+    sys.exit(1)
+
+subprocess.call(["/usr/local/apache2/bin/apachectl", "-f", f"/home/proj2/.confs/httpd{conf}.conf"])
