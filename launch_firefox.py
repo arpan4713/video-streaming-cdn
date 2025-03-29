94ca330dd31c6a7eba4803419583c4a5ac1f8387
arpan4713 <153415169+arpan4713@users.noreply.github.com>
2025-03-29 10:50:21 +0530

    Update launch_firefox.py

--- launch_firefox.py
+++ launch_firefox.py
@@ -3,9 +3,10 @@ import sys
 import os
 
 if len(sys.argv) != 2:
-	print 'Error: Usage is python launch_firefox.py <profile_num>'
-	exit(1)
+    print('Error: Usage is python3 launch_firefox.py <profile_num>')
+    sys.exit(1)
 
 profileNum = sys.argv[1]
-devNull = open(os.devnull, 'w')
-subprocess.call(["firefox", "-P", "eecs489profile" + profileNum], stdout=devNull, stderr=devNull)
+
+with open(os.devnull, 'w') as devNull:
+    subprocess.call(["firefox", "-P", f"eecs489profile{profileNum}"], stdout=devNull, stderr=devNull)
